7ccd9c7dc21ba1a4a6e468363572a511f8217c4b
Epicmoofish <etbender@icloud.com>
2023-11-10 00:29:55 -0500

    test

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBKHr8la5rOVmbGMDhAPvDPtlmacqFBcIA",
-  authDomain: "dharmannbingoboard.firebaseapp.com",
-  projectId: "dharmannbingoboard",
-  storageBucket: "dharmannbingoboard.appspot.com",
-  messagingSenderId: "402567985467",
-  appId: "1:402567985467:web:39b898e9785dd20ad01f1e",
-  measurementId: "G-L45KBQDD45"
-};
-
-// Initialize Firebase
-const appe = initializeApp(firebaseConfig);
-const analytics = getAnalytics(appe);
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
